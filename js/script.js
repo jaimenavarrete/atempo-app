87ef41bd0556b484678925b96d04ec8b3688f1a1
Jaime Navarrete <jaimenava123@outlook.com>
2021-05-21 13:51:41 -0600

    Finalizados - Últimos ajustes para el funcionamiento del menú y de la barra de búsqueda

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@
 let boton_menu = document.getElementById('boton_menu'),
     menu_principal = document.getElementById('menu_principal');
 
-boton_menu.addEventListener('click', () => {
+boton_menu.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Si la barra de búsqueda está abierta, la cierra al abrir el menú
+
+    if (barra_busqueda.className.match('barra_busqueda_activa')) {
+        boton_buscar.click();
+    }
+
     menu_principal.classList.toggle('menu_activo');
 });
 
@@ -12,6 +20,14 @@ boton_menu.addEventListener('click', () => {
 let boton_buscar = document.getElementById('boton_buscar'),
     barra_busqueda = document.getElementById('contenedor_barra_busqueda');
 
-boton_buscar.addEventListener('click', () => {
+boton_buscar.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Si el menú está abierto, lo cierra al abrir la barra de búsqueda
+
+    if (menu_principal.className.match('menu_activo')) {
+        boton_menu.click();
+    }
+
     barra_busqueda.classList.toggle('barra_busqueda_activa');
 });
